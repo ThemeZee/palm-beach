9e35dd95ec84d17691a4109d2581d7d754bb58db
Thomas W <netzberufler@googlemail.com>
2016-06-27 17:30:00 +0200

    added responsive layout

--- functions.php
+++ functions.php
@@ -111,7 +111,7 @@ function palm_beach_widgets_init() {
 	register_sidebar( array(
 		'name' => esc_html__( 'Sidebar', 'palm-beach' ),
 		'id' => 'sidebar',
-		'description' => esc_html__( 'Appears on single posts and pages.', 'palm-beach' ),
+		'description' => esc_html__( 'Appears on posts and pages except the full width template.', 'palm-beach' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s clearfix">',
 		'after_widget' => '</aside>',
 		'before_title' => '<div class="widget-header"><h3 class="widget-title">',
