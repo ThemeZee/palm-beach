b2c602321f8cc57797e415b3e58dcb59599cc1d5
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-05-15 16:08:24 +0200

    fixed small live preview issue in Customizer

--- inc/magazine.php
+++ inc/magazine.php
@@ -58,7 +58,7 @@ function palm_beach_get_magazine_post_ids( $cache_id, $category, $number_of_post
 	$cache_id = sanitize_key( $cache_id );
 	$post_ids = get_transient( 'palm_beach_magazine_post_ids' );
 
-	if ( ! isset( $post_ids[ $cache_id ] ) ) {
+	if ( ! isset( $post_ids[ $cache_id ] ) || is_customize_preview() ) {
 
 		// Get Posts from Database.
 		$query_arguments = array(
