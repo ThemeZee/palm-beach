f46bc2ff4cb893319f3ef84a7e1902680bf4f52a
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-11-18 10:53:39 +0100

    improved styling of Gutenberg Blocks

--- functions.php
+++ functions.php
@@ -214,7 +214,7 @@ add_action( 'enqueue_block_editor_assets', 'palm_beach_custom_fonts', 1 );
  * Enqueue editor styles for the new Gutenberg Editor.
  */
 function palm_beach_block_editor_assets() {
-	wp_enqueue_style( 'palm-beach-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20181102', 'all' );
+	wp_enqueue_style( 'palm-beach-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20191118', 'all' );
 }
 add_action( 'enqueue_block_editor_assets', 'palm_beach_block_editor_assets' );
 
