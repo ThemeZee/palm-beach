d2b160f6d222eb732a898bed88fad7d5aeee6676
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-01-16 13:28:16 +0100

    added Safari flexbox stylesheet

--- functions.php
+++ functions.php
@@ -177,6 +177,9 @@ function palm_beach_scripts() {
 	// Register and Enqueue Stylesheet.
 	wp_enqueue_style( 'palm-beach-stylesheet', get_stylesheet_uri(), array(), $theme_version );
 
+	// Register and Enqueue Safari Flexbox CSS fixes.
+	wp_enqueue_style( 'palm-beach-safari-flexbox-fixes', get_template_directory_uri() . '/assets/css/safari-flexbox-fixes.css', array(), '20210116' );
+
 	// Register Genericons.
 	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/assets/genericons/genericons.css', array(), '3.4.1' );
 
