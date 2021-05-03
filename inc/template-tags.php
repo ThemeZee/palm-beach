a1a1a732a533f9239fd1facbd280b1545257c81c
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 18:09:04 +0200

    fixed typo in pagination function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -390,7 +390,7 @@ if ( ! function_exists( 'palm_beach_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'palm-beach' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'palm-beach' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'palm-beach' ) . '</span>&raquo;',
 		) );
 	}
