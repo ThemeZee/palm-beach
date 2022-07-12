1a64e8a22bc0e3d3fb4d7d83cc9eda65f06ad6d8
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-07-12 10:06:53 +0200

    added theme support for custom spacing and line heights

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -13,6 +13,12 @@
  */
 function palm_beach_gutenberg_support() {
 
+	// Add theme support for dimension controls.
+	add_theme_support( 'custom-spacing' );
+
+	// Add theme support for custom line heights.
+	add_theme_support( 'custom-line-height' );
+
 	// Define block color palette.
 	$color_palette = apply_filters( 'palm_beach_color_palette', array(
 		'primary_color'    => '#57b7d7',
